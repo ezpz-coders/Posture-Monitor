ffa3dd02c4b18f4abac80479a4b7e87ddb8ebcb9
lakshit k. singh <66665569+LakshitXD@users.noreply.github.com>
2021-04-24 23:09:49 +0530

    added tkinter app

--- src/index.js
+++ src/index.js
@@ -41,6 +41,23 @@ app.on('activate', () => {
     createWindow();
   }
 });
+const { spawn } = require('child_process');
+
+
+const childPython = spawn('python', ["main.py"]);
+
+childPython.stdout.on('data',(data)=> {
+	console.log(`stdout: ${data}`);
+});
+
+childPython.stderr.on('data', (data) =>{
+	console.log(`stderr: ${data}`);
+});
+
+childPython.on('close', (code) =>{
+	console.log(`child process exited with code ${code}`);
+});
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
